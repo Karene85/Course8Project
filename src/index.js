045921f6b36fdbfe48bae42e5ff7a342b17526d3
karene85 <karenemody@gmail.com>
2021-03-04 17:45:53 +0530

    created react app and imported initial stub code

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <span>
+      Image Viewer
+  </span>, 
+  document.getElementById('root')
+);
+
+
