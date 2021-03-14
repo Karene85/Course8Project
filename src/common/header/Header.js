cb834697664704028538919c539f88f5f1b24ec8
karene85 <karenemody@gmail.com>
2021-03-14 18:57:52 +0530

    Added profile picture dropdown menu with corresponding onClick handlers

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import Input from '@material-ui/core/Input';
 import Typography from '@material-ui/core/Typography';
 import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
 import './Header.css';
+import Profile from '../../screens/profile/Profile';
+import Login from '../../screens/login/Login';
 
 
 class Header extends Component {
@@ -10,9 +16,30 @@ class Header extends Component {
     constructor() {
         super();
         this.state = {
-            profile_picture: "http://www.siggraph.org/sites/default/files/styles/full_news/public/field/image/women-code.jpg?itok=5Sr7oxlt"
+            profile_picture: "http://www.siggraph.org/sites/default/files/styles/full_news/public/field/image/women-code.jpg?itok=5Sr7oxlt",
+            searchInput: "",
+            showProfilePicMenu: false
         }
     }
+
+    searchInputChangeHandler = (e) => {
+        this.setState({searchInput: e.target.value});
+    }
+
+    profilePicClickHandler = () => {
+        this.setState({showProfilePicMenu: true});
+    }
+
+    logoutClickHandler = () => {
+        sessionStorage.clear();
+        ReactDOM.render(<Login />, document.getElementById('root'));
+    }
+
+    myAccountClickHandler = () => {
+        ReactDOM.render(<Profile />, document.getElementById('root'));
+    }
+
+
 
     render() {
         return(
@@ -25,7 +52,7 @@ class Header extends Component {
                         this.props.showSearchBox === 'true' ?
                         <div className='search-box'>
                             <SearchIcon />
-                            <Input disableUnderline placeholder='Search...'></Input>
+                            <Input disableUnderline placeholder='Search...' onChange={this.searchInputChangeHandler}></Input>
                         </div>
                         : ""
                     }
@@ -33,7 +60,22 @@ class Header extends Component {
                     {
                         this.props.showProfilePic === 'true' ?
                         <div className='profile-pic'>
-                            <img src={this.state.profile_picture} id='profile-pic-img' />
+                            <IconButton onClick={this.profilePicClickHandler}>
+                            <img id='profile-pic-img' src={this.state.profile_picture}  />
+                            </IconButton>
+                            
+                            {
+                                this.state.showProfilePicMenu ? (
+                                    <div className='profile-pic-menu'>
+                                    <MenuList>
+                                        <MenuItem onClick={this.myAccountClickHandler}>My Account</MenuItem>
+                                        <MenuItem onClick={this.logoutClickHandler}>Logout</MenuItem>
+                                    </MenuList>
+                                    </div>
+                                ) : (null)
+                            }
+                                                     
+                                                        
                         </div>
                         : ""
                     }
